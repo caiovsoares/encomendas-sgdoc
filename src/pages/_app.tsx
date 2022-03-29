19c59315a7b1ba3d7a42442f2ac1c081185332bf
caiovsoares <caio.vasoares@gmail.com>
2022-03-29 15:51:16 -0300

    Inserção de dados fantasmas e inicio da estilização da tabela

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,15 +1,17 @@
-import { Button, ChakraProvider } from '@chakra-ui/react'
+import { ChakraProvider } from '@chakra-ui/react'
 
 import theme from '../theme'
 import { AppProps } from 'next/app'
 
 import { Container } from '../components/Container'
+import { DarkModeSwitch } from '../components/DarkModeSwitch'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ChakraProvider resetCSS theme={theme}>
       <Container >
         <Component {...pageProps} />
+        <DarkModeSwitch/>
       </Container>
     </ChakraProvider>
   )
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,19 +1,44 @@
-import {
-  Link as ChakraLink,
-  Text,
-  Code,
-  List,
-  ListIcon,
-  ListItem,
-  Button,
-} from '@chakra-ui/react'
+import { Button, Flex, Table, Tbody, Td, Th, Thead, Tr } from '@chakra-ui/react'
+import { exampleMails } from '../utils'
+import { useTable, useSortBy } from 'react-table'
 
-import { CheckCircleIcon, LinkIcon } from '@chakra-ui/icons'
-import { Container } from '../components/Container'
-import { DarkModeSwitch } from '../components/DarkModeSwitch'
-
-const Index = () => (
-  <DarkModeSwitch/>
+const Index = (props) => (
+  <Flex width='100%' flexDir='column'>
+    <Flex flexDir='row' h='10vh'>
+      <Button>oi</Button>
+    </Flex>
+    <Flex flexDir='row' alignItems='center' h='80vh' width='100%' overflowY='scroll'>
+      <Table variant="simple">
+        <Thead>
+          <Tr>
+            <Th>Rastreio</Th>
+            <Th>Remetente</Th>
+            <Th>Destinatário</Th>
+            <Th>Recebido</Th>
+          </Tr>
+        </Thead>
+        <Tbody>
+          {props.exampleMails.map(e =>
+            <Tr>
+              <Td>{e.tracking}</Td>
+              <Td>{e.sender}</Td>
+              <Td>{e.destiny.warName}</Td>
+              <Td>{e.receiver.warName}</Td>
+            </Tr>)}
+        </Tbody>
+      </Table>
+    </Flex>
+    <Flex flexDir='row' h='10vh'>
+      <Button>oi</Button>
+    </Flex>
+    {/*props.exampleMails.map(e => <Button key={e.id}>{e.id+' '+e.destiny.fullName}</Button>)*/}
+  </Flex>
 )
+
+export async function getStaticProps(context) {
+  return {
+    props: {exampleMails},
+  }
+}
 
 export default Index
