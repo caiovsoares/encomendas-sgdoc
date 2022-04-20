08507f71e391c33d91e6b13c92a3b0ec270c0a4e
caiovsoares <caio.vasoares@gmail.com>
2022-04-20 13:29:01 -0300

    Auth

--- src/pages/encomendas.tsx
+++ src/pages/encomendas.tsx
@@ -1,126 +1,152 @@
-import React, { useState } from 'react'
-import { Box, Flex, Text, useDisclosure } from '@chakra-ui/react'
-import { exampleMails, proxyConfig } from '../utils'
-import ReactTable from '../components/ReactTable'
-import { PageButton } from '../components/PageButton'
-import { BiCheckCircle, BiInfoCircle, BiXCircle } from 'react-icons/bi'
-import MailsModal from '../components/MailsModal'
-import axios from 'axios'
-import { getSession } from 'next-auth/react'
+import React, { useState } from "react";
+import { Box, Flex, Text, useDisclosure } from "@chakra-ui/react";
+import { exampleMails } from "../utils";
+import ReactTable from "../components/ReactTable";
+import { PageButton } from "../components/PageButton";
+import { BiCheckCircle, BiInfoCircle, BiXCircle } from "react-icons/bi";
+import MailsModal from "../components/MailsModal";
+import axios from "axios";
+import { getSession } from "next-auth/react";
 
 const Encomendas = (props) => {
-
-  const { isOpen, onOpen, onClose } = useDisclosure()
-  const [mail, setMail] = useState({})
-  const [modalType, setModalType] = useState('')
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const [mail, setMail] = useState({});
+  const [modalType, setModalType] = useState("");
 
   async function HandleDetailItem(mail: {}) {
-    onOpen()
-    setMail(mail)
-    setModalType('detail')
+    onOpen();
+    setMail(mail);
+    setModalType("detail");
   }
 
   function HandleRegisterItem(mail: {}) {
-    onOpen()
-    setMail(mail)
-    setModalType('register')
+    onOpen();
+    setMail(mail);
+    setModalType("register");
   }
 
   function HandleReceiveItens(mail: {}) {
-    onOpen()
-    setMail(mail)
-    setModalType('receive')
+    onOpen();
+    setMail(mail);
+    setModalType("receive");
   }
 
   function HandleSearchItens(mail: {}) {
-    onOpen()
-    setMail(mail)
-    setModalType('search')
+    onOpen();
+    setMail(mail);
+    setModalType("search");
   }
 
   const columns = React.useMemo(
     () => [
       {
-        Header: 'Rastreio',
-        accessor: 'tracking',
+        Header: "Rastreio",
+        accessor: "tracking",
       },
       {
-        Header: 'Remetente',
-        accessor: 'sender',
+        Header: "Remetente",
+        accessor: "sender",
       },
       {
-        Header: 'Destinatário',
-        accessor: 'destiny.warName',
+        Header: "Destinatário",
+        accessor: "destiny.warName",
       },
       {
-        Header: 'Recebido',
-        accessor: 'receiver.warName',
+        Header: "Recebido",
+        accessor: "receiver.warName",
         Cell: ({ cell: { value } }) => {
           return (
-            <>{value ?
-              <Flex flexDir='row'><Box mr='5px' flexDir='row'><BiCheckCircle color='green' size='20px' /></Box>{String(value)}</Flex> :
-              <Box mr='5px'><BiXCircle color='red' size='20px' /></Box>}
+            <>
+              {value ? (
+                <Flex flexDir="row">
+                  <Box mr="5px" flexDir="row">
+                    <BiCheckCircle color="green" size="20px" />
+                  </Box>
+                  {String(value)}
+                </Flex>
+              ) : (
+                <Box mr="5px">
+                  <BiXCircle color="red" size="20px" />
+                </Box>
+              )}
             </>
-          )
-        }
+          );
+        },
       },
       {
-        Header: ' ',
-        accessor: row => row,
+        Header: " ",
+        accessor: (row) => row,
         Cell: ({ cell: { value } }) => {
           return (
-            <Flex alignItems='center' h='20px'>
-              <BiInfoCircle size='30px' onClick={() => { HandleDetailItem(value) }} />
+            <Flex alignItems="center" h="20px">
+              <BiInfoCircle
+                size="30px"
+                onClick={() => {
+                  HandleDetailItem(value);
+                }}
+              />
             </Flex>
-          )
-        }
-      }
+          );
+        },
+      },
     ],
     []
-  )
+  );
 
-  const data = React.useMemo(() => props.mails, [])
+  const data = React.useMemo(() => props.mails, []);
 
   return (
-
-    <Flex width='100%' flexDir='column'>
-      <MailsModal isOpen={isOpen} onClose={onClose} mail={mail} type={modalType} />
-      <Flex flexDir='row' minH='30px' h='7vh' alignItems='center' justifyContent='center'>
+    <Flex width="100%" flexDir="column">
+      <MailsModal
+        isOpen={isOpen}
+        onClose={onClose}
+        mail={mail}
+        type={modalType}
+      />
+      <Flex
+        flexDir="row"
+        minH="30px"
+        h="7vh"
+        alignItems="center"
+        justifyContent="center"
+      >
         <PageButton>Novo Cadastro</PageButton>
         <PageButton>Buscar</PageButton>
         <PageButton>Registrar Recebimento</PageButton>
       </Flex>
-      <Flex flexDir='row' alignItems='center' h='93vh' width='100%'>
+      <Flex flexDir="row" alignItems="center" h="93vh" width="100%">
         <ReactTable columns={columns} data={data} />
       </Flex>
     </Flex>
-  )
-}
+  );
+};
 
 export async function getServerSideProps(context) {
-
-  //const mails = exampleMails(100);
   const session = await getSession(context);
 
   if (!session || !session.user.permission.editMail)
     return {
       redirect: {
-        destination: '/',
+        destination: "/",
         permanent: false,
       },
-    }
+    };
 
   const fromDate = context.query.from;
   const toDate = context.query.to;
 
-  //const mails = await (await axios.get(`http://localhost:3333/mails/findAll?userId=${session.user.id}&from=${fromDate}&to=${toDate}`,{proxy:proxyConfig})).data;
-
-  const mails = exampleMails(300);
-  
+  let mails;
+  if (process.env.ENVIRONMENT == "DEV")
+    mails = await (
+      await axios.get(
+        `${process.env.API_URL}/mails/findAll?userId=${session.user.id}&from=${fromDate}&to=${toDate}`
+      )
+    ).data;
+  else mails = exampleMails(300);
 
   return {
     props: { mails },
-  }
+  };
 }
 
-export default Encomendas
+export default Encomendas;
--- src/utils/index.tsx
+++ src/utils/index.tsx
@@ -1,65 +1,76 @@
 const gerador = (str1, str2, str3) => {
-    const n = ((Math.random() * 10 + 1) % 2).toFixed(0)
-    switch (n) {
-        case '0':
-            return str1
-        case '1':
-            return str2
-        case '2':
-            return str3
-    }
-}
+  const n = ((Math.random() * 10 + 1) % 2).toFixed(0);
+  switch (n) {
+    case "0":
+      return str1;
+    case "1":
+      return str2;
+    case "2":
+      return str3;
+  }
+};
 
 export const exampleMails = (q) => {
+  let bigMails = new Array(q);
+  bigMails.fill(1);
+  bigMails = bigMails.map((e, i) => {
+    return {
+      id: i,
+      tracking: `BR${(Math.random() * 899999999 + 100000000).toFixed(0)}BR`,
+      type: gerador("Caixa", "Pacote", "Envelope"),
+      size: gerador("P", "M", "G"),
+      sender: gerador("Mercado Livre", "Amazon", "Shopee"),
+      destiny: {
+        id: i,
+        fullName: gerador(
+          "Caio Vinicius Amancio Soares",
+          "Gabriel Silva Ramos",
+          "Clarice Outrora Outronome"
+        ),
+        warName: gerador("C4 Mariane Mocker", "C3 Fagundes", "C2 Victoria"),
+        classYear: gerador("2020", "2021", "2022"),
+        cpf: gerador("126.882.218-33", "377.466.108-11", "430.103.428-53"),
+        identity: gerador("20YS0162", "21YS0241", "22YS0121"),
+      },
+      created_at: gerador(
+        "2021-06-25 14:35:22",
+        "2021-06-26 09:35:22",
+        "2021-04-12 12:15:22"
+      ),
+      received_at: gerador(
+        "2021-08-03 16:45:22",
+        "2021-07-02 14:22:22",
+        "2021-11-11 13:05:22"
+      ),
+      receiver: gerador(
+        {
+          id: i,
+          fullName: gerador(
+            "Caio Vinicius Amancio Soares",
+            "Gabriel Silva Ramos",
+            "Clarice Outrora Outronome"
+          ),
+          warName: gerador("C4 Mariane Mocker", "C3 Fagundes", "C2 Victoria"),
+          classYear: gerador("2020", "2021", "2022"),
+          cpf: gerador("126.882.218-33", "377.466.108-11", "430.103.428-53"),
+          identity: gerador("20YS0162", "21YS0241", "22YS0121"),
+        },
+        {
+          id: i,
+          fullName: gerador(
+            "Caio Vinicius Amancio Soares",
+            "Gabriel Silva Ramos",
+            "Clarice Outrora Outronome"
+          ),
+          warName: gerador("C4 Mariane Mocker", "C3 Fagundes", "C2 Victoria"),
+          classYear: gerador("2020", "2021", "2022"),
+          cpf: gerador("126.882.218-33", "377.466.108-11", "430.103.428-53"),
+          identity: gerador("20YS0162", "21YS0241", "22YS0121"),
+        },
+        null
+      ),
+    };
+  });
 
-    let bigMails = new Array(q)
-    bigMails.fill(1)
-    bigMails = bigMails.map((e, i) => {
-        return (
-            {
-                id: i,
-                tracking: `BR${(Math.random() * 899999999 + 100000000).toFixed(0)}BR`,
-                type: gerador('Caixa', 'Pacote', 'Envelope'),
-                size: gerador('P', 'M', 'G'),
-                sender: gerador('Mercado Livre', 'Amazon', 'Shopee'),
-                destiny: {
-                    id: i,
-                    fullName: gerador('Caio Vinicius Amancio Soares', 'Gabriel Silva Ramos', 'Clarice Outrora Outronome'),
-                    warName: gerador('C4 Mariane Mocker', 'C3 Fagundes', 'C2 Victoria'),
-                    classYear: gerador('2020', '2021', '2022'),
-                    cpf: gerador('126.882.218-33', '377.466.108-11', '430.103.428-53'),
-                    identity: gerador('20YS0162', '21YS0241', '22YS0121')
-                },
-                created_at: gerador('2021-06-25 14:35:22', '2021-06-26 09:35:22', '2021-04-12 12:15:22'),
-                received_at: gerador('2021-08-03 16:45:22', '2021-07-02 14:22:22', '2021-11-11 13:05:22'),
-                receiver: gerador({
-                    id: i,
-                    fullName: gerador('Caio Vinicius Amancio Soares', 'Gabriel Silva Ramos', 'Clarice Outrora Outronome'),
-                    warName: gerador('C4 Mariane Mocker', 'C3 Fagundes', 'C2 Victoria'),
-                    classYear: gerador('2020', '2021', '2022'),
-                    cpf: gerador('126.882.218-33', '377.466.108-11', '430.103.428-53'),
-                    identity: gerador('20YS0162', '21YS0241', '22YS0121')
-                }, {
-                    id: i,
-                    fullName: gerador('Caio Vinicius Amancio Soares', 'Gabriel Silva Ramos', 'Clarice Outrora Outronome'),
-                    warName: gerador('C4 Mariane Mocker', 'C3 Fagundes', 'C2 Victoria'),
-                    classYear: gerador('2020', '2021', '2022'),
-                    cpf: gerador('126.882.218-33', '377.466.108-11', '430.103.428-53'),
-                    identity: gerador('20YS0162', '21YS0241', '22YS0121')
-                }, null)
-            }
-        )
-    })
-
-    return bigMails
-}
-
-export const proxyConfig = {
-    host: 'proxy.gapys.intraer',
-    port: 8080,
-    auth: {
-        username: '46077534838',
-        password: 'Opticom1!'
-    }
-
-}
+  return bigMails;
+};
