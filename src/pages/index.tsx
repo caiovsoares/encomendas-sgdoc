43b65abca9e1be4f72b27948054a4159d37bfb63
caiovsoares <caio.vasoares@gmail.com>
2022-04-21 14:19:58 -0300

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,56 +1,64 @@
-import  React  from 'react'
-import { Button, Flex, Table, Tbody, Td, Th, Thead, Tr } from '@chakra-ui/react'
-import { exampleMails } from '../utils'
-import ReactTable from '../components/ReactTable'
-
+import React from 'react';
+import {
+  Button,
+  Flex,
+  Table,
+  Tbody,
+  Td,
+  Th,
+  Thead,
+  Tr,
+} from '@chakra-ui/react';
+import { exampleMails } from '../utils';
+import ReactTable from '../components/ReactTable';
 
 const Index = (props) => {
-
   const columns = React.useMemo(
     () => [
-          {
-            Header: 'Rastreio',
-            accessor: 'tracking',
-          },
-          {
-            Header: 'Remetente',
-            accessor: 'sender',
-          },
-          {
-            Header: 'Destinatário',
-            accessor: 'destiny.warName',
-          },
-          {
-            Header: 'Recebido',
-            accessor: 'receiver.warName',
-          },
-        ],
+      {
+        Header: 'Rastreio',
+        accessor: 'tracking',
+      },
+      {
+        Header: 'Remetente',
+        accessor: 'sender',
+      },
+      {
+        Header: 'Destinatário',
+        accessor: 'destiny.warName',
+      },
+      {
+        Header: 'Recebido',
+        accessor: 'receiver.warName',
+      },
+    ],
     []
-  )
-  
-  const data = React.useMemo(() => props.mails, [])
-  
-  return(
-  <Flex width='100%' flexDir='column'>
-    <Flex backgroundColor='red' flexDir='row' h='7vh'>
-      <Button>oi</Button>
+  );
+
+  const data = React.useMemo(() => props.mails, []);
+
+  return (
+    <Flex width='100%' flexDir='column'>
+      <Flex backgroundColor='red' flexDir='row' h='7vh'>
+        <Button>oi</Button>
+      </Flex>
+      <Flex flexDir='row' alignItems='center' h='86vh' width='100%'>
+        {/* <ReactTable columns={columns} data={data} /> */}
+      </Flex>
+      <Flex backgroundColor='red' flexDir='row' h='7vh'>
+        <Button>oi</Button>
+      </Flex>
+      1
     </Flex>
-    <Flex flexDir='row' alignItems='center' h='86vh' width='100%'>
-      <ReactTable columns={columns} data={data}/>
-    </Flex>
-    <Flex backgroundColor='red' flexDir='row' h='7vh'>
-      <Button>oi</Button>
-    </Flex>1
-  </Flex>
-)}
+  );
+};
 
 export async function getServerSideProps(context) {
-
   const mails = exampleMails(100);
 
   return {
-    props: {mails},
-  }
+    props: { mails },
+  };
 }
 
-export default Index
+export default Index;
