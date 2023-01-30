70bda90be91524dc52548cd58064a8ecae364227
caiovsoares <caio.soares@sou.fae.br>
2023-01-30 15:35:45 -0300

    Insert Tutorial

--- src/components/ReceiversModal/ShowTutorial.tsx
+++ src/components/ReceiversModal/ShowTutorial.tsx
@@ -35,13 +35,37 @@ export const ShowTutorial = () => {
               sistema utilizando a primeira linha como cabeçalho com as
               seguintes informações:
             </Text>
-            <Text ml='40px'>fullName, warName, classYear, cpf, identity</Text>
-            <Text ml='40px' color='alert'>
-              É importante que o cabeçalho(linha 1) possua exatamente essa mesma
-              configuração
+            <Text ml='40px'>
+              •Para inserir diversos{' '}
+              <strong>
+                CADETES: nome completo, nome de guerra, cpf, identidade, ano
+              </strong>
             </Text>
-            <Text>Exemplo:</Text>
-            <Image src='/tutorial/tutorial1.png' />
+            <Text ml='40px'>
+              •Para inserir diversos{' '}
+              <strong>
+                MILITARES: nome completo, nome de guerra, cpf, identidade, posto
+              </strong>
+            </Text>
+            <Text ml='40px'>
+              •Para inserir diversos <strong>SETORES: nome, sigla</strong>
+            </Text>
+            <Text ml='40px' color='alert' bg='yellow' w='fit-content'>
+              <strong>
+                É importante que o cabeçalho(linha 1) possua exatamente essa
+                mesma configuração
+              </strong>
+            </Text>
+            <Text>Exemplo Cadete:</Text>
+            <Image src='/tutorial/exemploCadete.png' />
+            <Text>Exemplo Militar:</Text>
+            <Text>
+              Os valores válidos para posto são: S2, S1, CB, 3S, 2S, 1S, SO,
+              ASP, 2T, 1T, CAP, MAJ, TC, CEL, BR, MBR, TBR, MAR
+            </Text>
+            <Image src='/tutorial/exemploMilitar.png' />
+            <Text>Exemplo Setor:</Text>
+            <Image src='/tutorial/exemploSetor.png' />
             <Heading size='xl' mt='40px'>
               2. Salvar
             </Heading>
@@ -51,9 +75,20 @@ export const ShowTutorial = () => {
             <Text ml='40px'>.xlsx .xls ou .csv</Text>
             <Text>Exemplos:</Text>
             <Flex>
-              <Image boxSize='35vw' src='tutorial/tutorial2.png' />
-              <Image boxSize='35vw' src='tutorial/tutorial3.png' ml='40px' />
+              <Image src='tutorial/salvar1.png' />
+              <Image src='tutorial/salvar2.png' ml='40px' />
             </Flex>
+            <Heading size='xl' mt='40px'>
+              3. Escolher o Arquivo e Enviar
+            </Heading>
+            <Text>
+              Clique em Escolher Arquivo e procure a planilha salva navegando
+              pelos arquivos de seu computador, abra e clique em enviar:
+            </Text>
+            <Text>Exemplos:</Text>
+            <Image src='tutorial/escolher.png' />
+            <Image src='tutorial/abrir.png' mt={3} />
+            <Image src='tutorial/enviar.png' mt={3} />
           </Box>
         </Box>
       </Slide>
